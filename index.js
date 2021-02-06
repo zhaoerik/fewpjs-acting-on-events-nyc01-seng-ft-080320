74909283cbdf64157c92e88e9d17827493948d86
Erik Zhao <erikzhao98@gmail.com>
2021-02-06 01:35:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function moveDodgerRight() {
   }
   
 let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
